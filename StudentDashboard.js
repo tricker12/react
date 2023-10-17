0c6c660728fab3da75a09586e0d5777a0ee2ebf8
tricker12 <147700242+tricker12@users.noreply.github.com>
2023-10-17 14:30:42 +0530

    Update StudentDashboard.js

--- StudentDashboard.js
+++ StudentDashboard.js
@@ -1,3 +1,5 @@
+
+
 import React, { useState } from 'react';
 import './styles.css';
 
@@ -8,6 +10,7 @@ const StudentDashboard = ({ students, setStudents, courses }) => {
   const [selectedCourse, setSelectedCourse] = useState('');
   const [editingStudent, setEditingStudent] = useState(null);
   const [filter, setFilter] = useState('');
+  const [addingStudent, setAddingStudent] = useState(false); // State for showing/hiding the add student form
 
   const addStudent = () => {
     if (studentName.trim() === '' || studentId.trim() === '' || studentClass.trim() === '' || selectedCourse.trim() === '') return;
@@ -41,6 +44,7 @@ const StudentDashboard = ({ students, setStudents, courses }) => {
     setStudentId('');
     setStudentClass('');
     setSelectedCourse('');
+    setAddingStudent(false); // Close the add student form
   };
 
   const editStudent = (student) => {
@@ -69,61 +73,135 @@ const StudentDashboard = ({ students, setStudents, courses }) => {
           value={filter}
           onChange={(e) => setFilter(e.target.value)}
         />
+        <button onClick={() => setAddingStudent(true)}>Add Student</button>
       </div>
-      <div>
-        <h2>{editingStudent ? 'Edit Student' : 'Add Student'}</h2>
-        <input
-          type="text"
-          placeholder="Student Name"
-          value={studentName}
-          onChange={(e) => setStudentName(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Student ID"
-          value={studentId}
-          onChange={(e) => setStudentId(e.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Student Class"
-          value={studentClass}
-          onChange={(e) => setStudentClass(e.target.value)}
-        />
-        <select
-          value={selectedCourse}
-          onChange={(e) => setSelectedCourse(e.target.value)}
-        >
-          <option value="" disabled>
-            Select a Course
-          </option>
-          {courses.map((course) => (
-            <option key={course.id} value={course.name}>
-              {course.name}
-            </option>
-          ))}
-        </select>
-        <button onClick={addStudent}>{editingStudent ? 'Update' : 'Add'}</button>
-        {editingStudent && (
-          <button onClick={() => setEditingStudent(null)}>Cancel</button>
-        )}
-      </div>
-      <div className="students-grid">
-        {filteredStudents.map((student) => (
-          <div className="student-card" key={student.id}>
-            <strong>Name: {student.name}</strong>
-            <p>Student ID: {student.studentId}</p>
-            <p>Class: {student.studentClass}</p>
-            <p>Course: {student.course}</p>
-            <div className="student-actions">
-              <button onClick={() => editStudent(student)}>Edit</button>
-              <button onClick={() => deleteStudent(student)}>Delete</button>
+
+      {/* Add Student Form Overlay */}
+      {addingStudent && (
+        <div className="add-student-overlay">
+          <div className="add-student-form">
+            <h2>Add Student</h2>
+            <input
+              type="text"
+              placeholder="Student Name"
+              value={studentName}
+              onChange={(e) => setStudentName(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Student ID"
+              value={studentId}
+              onChange={(e) => setStudentId(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Student Class"
+              value={studentClass}
+              onChange={(e) => setStudentClass(e.target.value)}
+            />
+            <select
+              value={selectedCourse}
+              onChange={(e) => setSelectedCourse(e.target.value)}
+            >
+              <option value="" disabled>Select a Course</option>
+              {courses.map((course) => (
+                <option key={course.id} value={course.name}>
+                  {course.name}
+                </option>
+              ))}
+            </select>
+            <div className="form-buttons">
+              <button onClick={addStudent}>Add</button>
+              <button onClick={() => setAddingStudent(false)}>Cancel</button>
             </div>
           </div>
-        ))}
+        </div>
+      )}
+
+      <div className="students-table">
+        <table>
+          <thead>
+            <tr>
+              <th>Student Name</th>
+              <th>Student ID</th>
+              <th>Student Class</th>
+              <th>Course</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {filteredStudents.map((student) => (
+              <tr key={student.id}>
+                <td>
+                  {editingStudent && editingStudent.id === student.id ? (
+                    <input
+                      type="text"
+                      value={studentName}
+                      onChange={(e) => setStudentName(e.target.value)}
+                    />
+                  ) : (
+                    student.name
+                  )}
+                </td>
+                <td>
+                  {editingStudent && editingStudent.id === student.id ? (
+                    <input
+                      type="text"
+                      value={studentId}
+                      onChange={(e) => setStudentId(e.target.value)}
+                    />
+                  ) : (
+                    student.studentId
+                  )}
+                </td>
+                <td>
+                  {editingStudent && editingStudent.id === student.id ? (
+                    <input
+                      type="text"
+                      value={studentClass}
+                      onChange={(e) => setStudentClass(e.target.value)}
+                    />
+                  ) : (
+                    student.studentClass
+                  )}
+                </td>
+                <td>
+                  {editingStudent && editingStudent.id === student.id ? (
+                    <select
+                      value={selectedCourse}
+                      onChange={(e) => setSelectedCourse(e.target.value)}
+                    >
+                      {courses.map((course) => (
+                        <option key={course.id} value={course.name}>
+                          {course.name}
+                        </option>
+                      ))}
+                    </select>
+                  ) : (
+                    student.course
+                  )}
+                </td>
+                <td>
+                  {editingStudent && editingStudent.id === student.id ? (
+                    <div className="form-buttons">
+                      <button onClick={addStudent}>Update</button>
+                      <button onClick={() => setEditingStudent(null)}>Cancel</button>
+                    </div>
+                  ) : (
+                    <div className="form-buttons">
+                      <button onClick={() => editStudent(student)}>Edit</button>
+                      <button onClick={() => deleteStudent(student)}>Delete</button>
+                    </div>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
 
 export default StudentDashboard;
+
